6fb639954d31d7728bff627593ee4e56ff358100
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-09-19 12:05:01 +0530

    Add files via upload

---
+++ D-10.py
@@ -0,0 +1,39 @@
+# Merge Sort
+
+arr = list(map(int,input("Enter the elements :").split()))
+
+def merge(arr, left , mid , right):
+    i = left
+    j = mid+1
+    temp = []
+    while i<=mid and j<=right:
+        if arr[i] <= arr[j]:
+            temp.append(arr[i])
+            i+=1
+        else:
+            temp.append(arr[j])
+            j+=1
+
+
+    while i<=mid:
+        temp.append(arr[i])
+        i+=1
+
+
+    while j <= right:
+        temp.append(arr[j])
+        j += 1
+
+        
+    for k in range(len(temp)):
+        arr[left + k] = temp[k]
+
+def mergesort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        mergesort(arr, left, mid)
+        mergesort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+mergesort(arr, 0, len(arr) - 1)
+print(arr)
