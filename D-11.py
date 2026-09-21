cbc582b0c3c08c5cb646e5c745f96452e6273c0d
yaradlaharshavardhanreddy-TurboS <yaradlaharshavardhanreddy@gmail.com>
2026-09-21 10:07:25 +0530

    Add files via upload

---
+++ D-11.py
@@ -0,0 +1,54 @@
+# Pan cake Sort
+
+'''
+
+def flip(arr,k):
+    i=0
+    j=k-1
+    while i<j:
+        arr[i],arr[j]=arr[j],arr[i]
+        i+=1
+        j+=1
+def pancakesort(arr):
+    for size in range(len(arr),1,-1):
+        max_index=0
+        for i in range(1,size):
+            if arr[i]>arr[max_index]:
+                max_index=i
+            flip(arr,max_index+1)
+            flip(arr,size)
+        return arr
+arr=list(map(int,input("Enter the elements: ").split()))
+print("sorted array:",pancakesort(arr))
+
+'''
+
+# Quick Sort
+
+
+def part(arr, low, high):
+    pivot = arr[high]
+    i = low-1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return i+1
+
+
+def quicksort(arr, low, high):
+    if low < high:
+        p = part(arr, low, high)
+        quicksort(arr, low, p-1)
+        quicksort(arr, p+1, high)
+
+arr=list(map(int,input("Enter the elements: ").split()))
+quicksort(arr, 0, len(arr)-1)
+print("sorted array:",arr)
+
+
+
+
+
+
